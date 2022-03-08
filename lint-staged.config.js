d1cfd6a9066075526ac84a0ab95f187c0c81dc0d
Осипчук Сергей Николаевич <sergei_osipchuk@epam.com>
2022-03-08 23:16:58 +0300

    feat: add airbnb config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  'src/**/*.(ts|tsx)': () => 'tsc --noEmit',
-  'src/**/*.(ts|tsx|js)': (filenames) => [`npm run lint --fix ${filenames.join(' ')}`, `npm run prettier-format --write ${filenames.join(' ')}`],
-  'src/**/*.(md|json)': (filenames) => `npm run prettier-format --write ${filenames.join(' ')}`,
+  'src/**/*.(ts|tsx)': () => 'npm run check-types',
+  'src/**/*.(ts|tsx|js)': () => ['npm run lint', 'npm run prettier-format'],
 };
